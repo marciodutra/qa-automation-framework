c6d6378f64b5c628c6ba73d3e2bb2f83dde4c8ce
Marcio Dutra <professormarciodutra@gmail.com>
2026-07-30 19:54:06 -0300

    feat: add Mochawesome reporter and improve test execution

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,17 @@
 const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const mochawesome = require("cypress-mochawesome-reporter/plugin");
+
 const {
   addCucumberPreprocessorPlugin,
 } = require("@badeball/cypress-cucumber-preprocessor");
+
 const {
   createEsbuildPlugin,
 } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 async function setupNodeEvents(on, config) {
+
   await addCucumberPreprocessorPlugin(on, config);
 
   on(
@@ -17,10 +21,28 @@ async function setupNodeEvents(on, config) {
     })
   );
 
+  mochawesome(on);
+
   return config;
 }
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+
+  reporterOptions: {
+  reportDir: "cypress/reports",
+  overwrite: false,
+  html: false,
+  json: true,
+
+  reportTitle: "QA Automation Framework",
+  reportPageTitle: "QA Automation Framework",
+  embeddedScreenshots: true,
+  inlineAssets: true,
+  charts: true,
+  saveAllAttempts: false
+},
+
   e2e: {
     baseUrl: "https://automationexercise.com",
     specPattern: "cypress/e2e/features/**/*.feature",
